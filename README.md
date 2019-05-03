21084531d97330f4673b8c8766652ce961984e61
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-05-03 12:38:05 +0200

    more logging. Typical logged example added to README

--- README.md
+++ README.md
@@ -49,4 +49,27 @@ The configuration looks like this:
     ; otherwise it is interpreted as a filename pointing to the ca certs.
     STSORGSYNC_CA_BUNDLE = true
 
+A typical logfile (with loglevel 20) should look somethat like this:
+
+    INFO 2019-05-03 12:01:19,485 mox_stsorgsync mox_stsorgsync starting
+    INFO 2019-05-03 12:01:19,567 mox_stsorgsync sync_stsorgsync_orgunits starting
+    INFO 2019-05-03 12:01:19,567 mox_stsorgsync sync_stsorgsync_orgunits getting all organisational units from os2mo
+    INFO 2019-05-03 12:01:20,245 mox_stsorgsync sync_stsorgsync_orgunits getting all organisational units from stsorgsync
+    INFO 2019-05-03 12:01:20,683 mox_stsorgsync sync_stsorgsync_orgunits deleting organisational units from stsorgsync if deleted in os2mo
+    INFO 2019-05-03 12:01:20,683 mox_stsorgsync sync_stsorgsync_orgunits upserting organisational units in stsorgsync
+    INFO 2019-05-03 12:01:21,973 mox_stsorgsync upsert orgunit aa0d5012-4320-4715-bb74-f087befefaa8
+    ...
+    INFO 2019-05-03 12:01:23,181 mox_stsorgsync upsert orgunit cf8cfc65-f6a6-4ca8-9812-f50322cc0ec9
+    INFO 2019-05-03 12:01:23,182 mox_stsorgsync sync_stsorgsync_orgunits done
+    INFO 2019-05-03 12:02:44,706 mox_stsorgsync sync_stsorgsync_users starting
+    INFO 2019-05-03 12:02:44,706 mox_stsorgsync sync_stsorgsync_users getting list of users from stsorgsync
+    INFO 2019-05-03 12:02:45,484 mox_stsorgsync sync_stsorgsync_users getting list of users from os2mo
+    INFO 2019-05-03 12:02:47,362 mox_stsorgsync sync_stsorgsync_users deleting os2mo-deleted users in stsorgsync
+    INFO 2019-05-03 12:02:47,362 mox_stsorgsync sync_stsorgsync_users upserting stsorgsync users
+    INFO 2019-05-03 12:02:48,024 mox_stsorgsync upsert user a1bc5918-a19b-4798-8788-ca43a97cd707
+    ...
+    INFO 2019-05-03 12:05:50,208 mox_stsorgsync upsert user f28b1d87-12b7-4555-a667-614a773ccc0f
+    INFO 2019-05-03 12:05:50,284 mox_stsorgsync sync_stsorgsync_users done
+    INFO 2019-05-03 12:05:50,284 mox_stsorgsync mox_stsorgsync done
+
 
--- mox_stsorgsync/__main__.py
+++ mox_stsorgsync/__main__.py
@@ -28,30 +28,51 @@ logger.setLevel(int(settings["MOX_LOG_LEVEL"]))
 
 
 def sync_stsorgsync_orgunits():
+    logger.info("sync_stsorgsync_orgunits starting")
+    logger.info("sync_stsorgsync_orgunits getting all organisational units from os2mo")
+
     os2mo_uuids = set(os2mo.org_unit_uuids())
+
+    logger.info("sync_stsorgsync_orgunits getting all organisational units from stsorgsync")
+
     stsorgsync_uuids = set(stsorgsync.orgunit_uuids())
 
     # delete from stsorgsync what is not in os2mo
+
+    logger.info("sync_stsorgsync_orgunits deleting organisational units from stsorgsync if deleted in os2mo")
+
     if len(os2mo_uuids):
         for uuid in set(stsorgsync_uuids - os2mo_uuids):
             stsorgsync.delete_orgunit(uuid)
+
+    logger.info("sync_stsorgsync_orgunits upserting organisational units in stsorgsync")
 
     for i in os2mo_uuids:
         sts_orgunit = os2mo.get_sts_orgunit(i)
         stsorgsync.upsert_orgunit(sts_orgunit)
 
+    logger.info("sync_stsorgsync_orgunits done")
 
 def sync_stsorgsync_users():
+    logger.info("sync_stsorgsync_users starting")
+    logger.info("sync_stsorgsync_users getting list of users from stsorgsync")
+
     stsorgsync_uuids = set(stsorgsync.user_uuids())
+
+    logger.info("sync_stsorgsync_users getting list of users from os2mo")
+
     os2mo_uuids = set(os2mo.user_uuids())
 
-    # delete from stsorgsync what is not in os2mo
+    logger.info("sync_stsorgsync_users deleting os2mo-deleted users in stsorgsync")
+
     if len(os2mo_uuids):
         for uuid in set(stsorgsync_uuids - os2mo_uuids):
             stsorgsync.delete_user(uuid)
 
     # insert/overwrite all users from os2mo
     # delete if user has no more positions
+    logger.info("sync_stsorgsync_users upserting stsorgsync users")
+
     for i in os2mo_uuids:
         sts_user = os2mo.get_sts_user(i)
 
@@ -62,11 +83,15 @@ def sync_stsorgsync_users():
 
         stsorgsync.upsert_user(sts_user)
 
+    logger.info("sync_stsorgsync_users done")
+
 
 if __name__ == "__main__":
+    logger.info("mox_stsorgsync starting")
     if not settings["OS2MO_ORG_UUID"]:
         settings["OS2MO_ORG_UUID"] = os2mo.os2mo_get("{BASE}/o/").json()[0][
             "uuid"
         ]
     sync_stsorgsync_orgunits()
     sync_stsorgsync_users()
+    logger.info("mox_stsorgsync done")
